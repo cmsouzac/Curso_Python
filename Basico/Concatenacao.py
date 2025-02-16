ed6fd4ab0e218bbb419a13da1e44a6ad04e0a941
Cristian Souza <cmsouzaac@outlook.com>
2025-02-15 22:53:28 -0300

    Concatenaçao

---
+++ Basico/Concatenacao.py
@@ -0,0 +1,29 @@
+# Description: Concatenacao de strings
+
+# 1. Concatenação é um processo de juntar duas ou mais strings
+
+# Concatenação de strings
+string1 = 'Olá'
+string2 = 'Mundo'
+resultado = string1 + ' ' + string2
+print(resultado)
+
+# Concatenação de Listas
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+resultado = lista1 + lista2
+print(resultado)
+
+# Repetição de strings
+string = 'Python'
+resultado = string * 3
+print(resultado)
+
+# Repeticao de listas
+lista = [1, 2]
+resultado = lista * 3
+print(resultado)
+
+# Combinando repetição e concatenação
+texto = "Oi" + " " + "Python" * 2
+print(texto)
