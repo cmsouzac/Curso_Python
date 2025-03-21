b744e6ccf3a457844bc7e1548480b8b1079daa68
Cristian Souza <cmsouzaac@outlook.com>
2025-03-21 16:58:26 -0300

    precedencias e strings

---
+++ Basico/Precedencia.py
@@ -0,0 +1,23 @@
+# 1. (n + n)
+# 2.  **
+# 3. * / //  %
+# 4. + -
+
+cont_1 = 1 + 1 ** 5 + 5
+print(cont_1) 
+
+# CALCULANDO IMC
+# medidas de peso IMC
+# Normal: 18.5 a 24.9
+# Sobrepeso: 25 a 29.9  
+# Obesidade: 30 a 39.9
+# Obesidade grave: 40 ou mais
+
+nome = "Cristian"
+peso = 64
+altura = 1.67
+imc = peso / altura ** 2 # 103 / (1.87 * 1.87) = 29.4
+print(nome, "seu IMC é: ", imc)
+print('Voçe, tem:', altura, "m")
+print('Voçe, tem:', peso, "kg")
+
--- Basico/strings.py
+++ Basico/strings.py
@@ -8,3 +8,19 @@ print(2, "Luiz 'Otavio'")
 
 #Escape
 print("Luiz \"Otavio\"")
+
+
+# Formatação de strings
+# f-string (Python 3.6+)
+nome = "Cristian"
+altura = 1.67
+peso = 64
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} seu IMC é: {imc:.2f}'
+linha_2 = f'Voçe, tem: {altura} m'
+linha_3 = f'Voçe, tem: {peso} kg'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
