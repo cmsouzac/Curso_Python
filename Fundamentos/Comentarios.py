4486ab0c734fca77546e47d161cfc67281258114
Cristian Souza <cmsouzaac@outlook.com>
2025-05-21 20:43:12 -0300

    nova entrada

---
+++ Fundamentos/Comentarios.py
@@ -0,0 +1,15 @@
+# Este é um comentário em Python
+print("Olá, mundo!")  # Este comentário está na mesma linha de código
+
+"""Este é um comentário de várias linhas
+    a = 1
+"""
+"""
+Este é um comentário de multiplas linhas
+vou fazer uma dissertação
+aqui!
+"""
+'''
+Mas eu nao preciso somente de aspas duplas
+posso usar aspas simples
+'''
---
+++ Fundamentos/FuncaoPrint.py
@@ -0,0 +1,4 @@
+print(12, 34, sep=" - ", end=" ")
+# O parâmetro sep define o separador entre os valores
+# O parâmetro end define o que será impresso no final
+print(56, 78, sep=" - ", end=" ")
